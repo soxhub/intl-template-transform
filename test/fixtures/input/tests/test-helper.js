99a7d03937760428a3cda861d51d0c291db24ccb
Chris Manson <chris@manson.ie>
2021-11-01 11:55:01 +0000

    add fixtures

---
+++ test/fixtures/input/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'input/app';
+import config from 'input/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
