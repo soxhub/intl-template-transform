a1a1479c21d70fdacb34ce85da5ba9d9c014e8ce
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2021-11-16 12:10:35 +0100

    Ability to exclude specific curly brackets components

---
+++ test/unit/link-to.js
@@ -0,0 +1,35 @@
+const testCase = require('../helpers/test-case');
+
+describe('Exclude curly components', function() {
+    testCase({
+        name: '{{link-to}} cannot be used in translation',
+        input: `
+        <span>
+            Click {{link-to "Contact Support" "zendesk.index"}}
+        </span>
+    `,
+        output: `
+        <span>
+            {{t "Click"}} {{link-to "Contact Support" "zendesk.index"}}
+        </span>
+    `
+    });
+
+    testCase({
+        name: 'Content of {{link-to}} should be translated',
+        input: `
+        <span>
+            {{#link-to "zendesk.index"}}
+                Click here
+            {{/link-to}}
+        </span>
+    `,
+        output: `
+        <span>
+            {{#link-to "zendesk.index"}}
+                {{t "Click here"}}
+            {{/link-to}}
+        </span>
+    `
+    });
+});
