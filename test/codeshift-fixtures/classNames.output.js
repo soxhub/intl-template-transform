2bb169ae41d65cb47e8804c5ca84aa7828934994
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2021-12-08 10:36:25 +0100

    Solve indempotence for js files

--- test/codeshift-fixtures/classNames.output.js
+++
@@ -1,3 +0,0 @@
-export default Component.extend({
-  classNames: ['aaa bbb', 'ccc']
-});
--- test/codeshift-fixtures/resizableHandles.output.js
+++
@@ -1,8 +0,0 @@
-export default class Container extends Component {
-  canDrag = false;
-  canResize = false;
-  canFullscreen = true;
-  isFocused = false;
-  isFullscreen = false;
-  resizableHandles = 'ne, nw, se, sw, n, e, s, w';
-}
