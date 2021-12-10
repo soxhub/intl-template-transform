192f738a9272f34e4246921977de17f63e378657
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2021-12-10 11:32:14 +0100

    Fixed convertToKey function (replace all occurences of this, {, }.

--- test/codeshift-fixtures/templateString.output.js
+++ test/codeshift-fixtures/templateString.output.js
@@ -3,16 +3,19 @@ export default class extends Component {
 	@service
 	intl;
 
+  c = 1;
+  d = 2;
 	constructor() {
 		super();
 
 		let a = 1;
 		let b = 2;
 		let message = this.intl.formatMessage({
-			defaultMessage: 'This {a} is {b} nice.',
+			defaultMessage: 'This {a} is {b} nice. {c_d}',
 		}, {
 			a: a,
 			b: b,
+      c_d: this.c + this.d,
 		});
 	}
 }
