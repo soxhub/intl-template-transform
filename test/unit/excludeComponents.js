d365afca21ddbf6fa7a94db39653e70b0c98dc2d
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2021-12-13 15:10:52 +0100

    Added zendesk-link as curly component.

---
+++ test/unit/excludeComponents.js
@@ -0,0 +1,33 @@
+const testCase = require('../helpers/test-case');
+
+describe('Helper Arguments', function () {
+  testCase({
+    name: 'Helper Arguments',
+    input: `
+    <span class="card-media-header capitalize">
+      {{audit.action}} {{listController.singularLabel}}
+    </span>
+    <span>
+      with &nbsp;{{clean-audit-field
+        field=audit.field
+      }}&nbsp;{{convert-audit-value
+        field=audit.field
+        idValue=audit.toValue
+      }}
+    </span>
+    `,
+    output: `
+    <span class="card-media-header capitalize">
+      {{audit.action}} {{listController.singularLabel}}
+    </span>
+    <span>
+      {{format-message "with &nbsp;" htmlSafe=true}}{{clean-audit-field
+        field=audit.field
+      }}&nbsp;{{convert-audit-value
+        field=audit.field
+        idValue=audit.toValue
+      }}
+    </span>
+    `
+  })
+})
