592ef8ed97c8e05754fead858a2bd928b60cd84c
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2021-12-14 13:05:50 +0100

    Excluded a few css styles from translations

---
+++ test/codeshift-fixtures/excludedCss.input.js
@@ -0,0 +1,25 @@
+function test(intl){
+  let styles = {
+    message: 'some css styles',
+  };
+
+  let aaa= {
+    someLogo: htmlSafe('some css styles'),
+    translated: 'some css styles',
+  };
+}
+
+function test2(intl){
+  let styles = function (){
+    return "some css styles";
+  };
+  let customClass = function (){
+    return "some css styles";
+  };
+  let sizes = function (){
+    return "some css styles";
+  };
+  let table_class_list = function (){
+    return "some css styles";
+  };
+}
