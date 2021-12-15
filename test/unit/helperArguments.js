97b9cc6ceccc5b0bc30c767bfa29e1d7e7d07ea1
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2021-12-15 13:41:59 +0100

    Escape { with '{ in translations

--- test/unit/helperArguments.js
+++ test/unit/helperArguments.js
@@ -3,10 +3,10 @@ const testCase = require('../helpers/test-case');
 describe('Helper Arguments', function () {
   testCase({
     name: 'Helper Arguments',
-    input: `<span title="{{fn this.testFunction 2.2}} website">
+    input: `<span title="{{fn this.testFunction 2.2 '{3}'}} website {4}">
         test
       </span>`,
-    output: `<span title={{format-message "{fn_testFunction_2_2} website" fn_testFunction_2_2=(fn this.testFunction 2.2)}}>
+    output: `<span title={{format-message "{fn_testFunction_2_2_3} website '{4'}" fn_testFunction_2_2_3=(fn this.testFunction 2.2 "{3}")}}>
         {{format-message "test"}}
       </span>`
   })
