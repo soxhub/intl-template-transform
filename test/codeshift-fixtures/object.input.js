4a21d62e273c3ee9ed3e8ea1dc7689c43e3aa5ef
Dave Laird <kiwiupover@gmail.com>
2021-12-15 11:21:46 -0800

    Adding an object comment test

---
+++ test/codeshift-fixtures/object.input.js
@@ -0,0 +1,16 @@
+
+const obj = {
+  message: "Nice Message",
+  message2: "Nice Message",
+  message3: [
+    "Ugly Message",
+  ],
+  message4: {
+    message: "Ugly Message",
+  },
+}
+
+stanObj = {
+  message: "Nice Man!",
+}
+
---
+++ test/codeshift-fixtures/object.output.js
@@ -0,0 +1,20 @@
+//TODO: intl-template-transform text must be inside ember class or inside a function with parameter intl, container, owner or intl can be defined inside the function.
+const obj = {
+  message: "Nice Message",
+
+  message2: "Nice Message",
+
+  message3: [
+    "Ugly Message",
+  ],
+
+  message4: {
+    message: "Ugly Message",
+  },
+}
+
+
+//TODO: intl-template-transform text must be inside ember class or inside a function with parameter intl, container, owner or intl can be defined inside the function.
+stanObj = {
+  message: "Nice Man!",
+};
