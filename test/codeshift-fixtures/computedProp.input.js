87b0119d7bbc1b9ff3f7a944d6605bcae281d4f5
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2021-12-15 13:34:15 +0100

    Fixed transform array to computed property

--- test/codeshift-fixtures/computedProp.input.js
+++ test/codeshift-fixtures/computedProp.input.js
@@ -1,5 +1,33 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-	message: "Nice Message"
+  message: "Nice Message",
+
+  message2: [
+    "Message"
+  ],
+
+  message2_2: {
+    message: "Message"
+  },
+
+  message3() {
+    return "Message";
+  },
+
+  message4: function () {
+    return "Message";
+  },
+
+  message5: () => {
+    return "Message";
+  },
+
+  message6: task(function* () {
+    return "Message";
+  }),
+
+  message7: computed(function* () {
+    return "Message";
+  })
 });
--- test/codeshift-fixtures/computedProp.output.js
+++ test/codeshift-fixtures/computedProp.output.js
@@ -3,11 +3,57 @@ import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 export default Component.extend({
-	intl: service('intl'),
+  intl: service('intl'),
 
-	message: computed('intl', function () {
-		return this.intl.formatMessage({
-			defaultMessage: 'Nice Message',
-		});
-	}),
+  message: computed(function () {
+    return this.intl.formatMessage({
+      defaultMessage: 'Nice Message',
+    });
+  }),
+
+  message2: computed(function () {
+    return [
+      this.intl.formatMessage({
+        defaultMessage: 'Message',
+      })
+    ];
+  }),
+
+  message2_2: computed(function () {
+    return {
+      message: this.intl.formatMessage({
+        defaultMessage: 'Message',
+      })
+    };
+  }),
+
+  message3() {
+    return this.intl.formatMessage({
+      defaultMessage: 'Message',
+    });
+  },
+
+  message4: function () {
+    return this.intl.formatMessage({
+      defaultMessage: 'Message',
+    });
+  },
+
+  message5: () => {
+    return this.intl.formatMessage({
+      defaultMessage: 'Message',
+    });
+  },
+
+  message6: task(function* () {
+    return this.intl.formatMessage({
+      defaultMessage: 'Message',
+    });
+  }),
+
+  message7: computed(function* () {
+    return this.intl.formatMessage({
+      defaultMessage: 'Message',
+    });
+  }),
 });
