996adf426da88be6959d44dcd907c06dd2a43cf0
Dave Laird <kiwiupover@gmail.com>
2021-12-26 19:40:28 -0800

    Update formatMessage to make the first argument a string

--- test/codeshift-fixtures/getPlaceholder.output.js
+++ test/codeshift-fixtures/getPlaceholder.output.js
@@ -1,7 +1,5 @@
 function test(intl) {
-  let message = intl.formatMessage({
-    defaultMessage: 'Hello {obj_name}',
-  }, {
+  let message = intl.formatMessage('Hello {obj_name}', {
     obj_name: get(obj, 'name'),
   });
 }
