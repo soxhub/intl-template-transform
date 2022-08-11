2526304dde395ef6423c0607c11b806af03b009a
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2022-08-11 12:58:19 +0200

    Escaped single quote

---
+++ test/unit/singleQuote.js
@@ -0,0 +1,13 @@
+const testCase = require("../helpers/test-case");
+
+describe("Single quote", function () {
+  testCase({
+    name: "Single quote should be escaped",
+    input: `
+      <span>I'm a span. Aren't tests awesome?</span>
+    `,
+    output: `
+      <span>{{format-message "I''m a span. Aren''t tests awesome?"}}</span>
+    `,
+  });
+});
