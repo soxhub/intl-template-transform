72d3d13e0806370c409b707586241f783eb5ed2d
Stanislav Dunajcan <stanislav.dunajcan@gmail.com>
2022-08-22 18:31:34 +0200

    Sox 17447 i18n - Pluralization with transform. (#76)
    Co-authored-by: Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>

---
+++ test/unit/pluralize.js
@@ -0,0 +1,33 @@
+const testCase = require("../helpers/test-case");
+
+describe("Translate pluralize", function () {
+  testCase({
+    name: "pluralize should be translated with counts",
+    input: `
+    Reopen {{pluralize this.selectedCount "Task" without-count=false}}
+    `,
+    output: `
+    {{format-message "Reopen {count, plural, =0 {no Tasks} =1 {one Task} other {# Tasks}}" count=this.selectedCount}}
+    `,
+  });
+
+  testCase({
+    name: "pluralize should be translated with counts, when no parameter",
+    input: `
+    Reopen {{pluralize this.selectedCount "Task"}}
+    `,
+    output: `
+    {{format-message "Reopen {count, plural, =0 {no Tasks} =1 {one Task} other {# Tasks}}" count=this.selectedCount}}
+    `,
+  });
+
+  testCase({
+    name: "pluralize should be translated without counts",
+    input: `
+    Reopen {{pluralize this.selectedCount "Task" without-count=true}}
+    `,
+    output: `
+    {{format-message "Reopen {count, plural, =0 {Tasks} =1 {Task} other {Tasks}}" count=this.selectedCount}}
+    `,
+  });
+});
